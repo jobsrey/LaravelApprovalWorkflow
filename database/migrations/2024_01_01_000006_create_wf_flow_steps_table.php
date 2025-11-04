03d5689d7d6249959c3325d8c71131f294123013
Rey <tatac53@email.issbox.com>
2025-11-04 10:34:01 +0700

    update             $table->timestamps();

--- database/migrations/2024_01_01_000006_create_wf_flow_steps_table.php
+++ database/migrations/2024_01_01_000006_create_wf_flow_steps_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,13 +16,14 @@ return new class extends Migration
             $table->unsignedBigInteger('flow_id');
             $table->string('name', 100)->nullable();
             $table->string('condition', 1000)->nullable();
-            
+            $table->timestamps();
+
             $table->foreign('flow_id')
                 ->references('id')
                 ->on('wf_flows')
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
-            
+
             $table->index(['flow_id', 'order']);
         });
     }
