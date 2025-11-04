ee12ee39639cb18fb2d6c1fca669c38dd72165d6
Rey <tatac53@email.issbox.com>
2025-11-04 10:10:18 +0700

    update

--- database/migrations/2024_01_01_000005_create_wf_flows_table.php
+++ database/migrations/2024_01_01_000005_create_wf_flows_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +16,8 @@ return new class extends Migration
             $table->unsignedBigInteger('company_id');
             $table->tinyInteger('is_active')->default(0);
             $table->string('label', 100)->nullable();
-            
+            $table->timestamps();
+
             $table->index(['company_id', 'type']);
         });
     }
